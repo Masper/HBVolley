bd1a8dc11893e76ddb31571bbd0b3686365950b4
Masper <casper.rang@gmail.com>
2019-12-14 15:51:59 +0100

    socket.io erbij server+client

---
+++ ioconnection.js
@@ -0,0 +1,23 @@
+const IP_ADDRESS = 'http://127.0.0.1'
+const PORT = '3000'
+
+class IOConnection {
+
+	constructor() {
+		this.socket = io.connect(IP_ADDRESS + ":" + PORT);
+		this.socket.on('connect', this.onConnect);
+		this.socket.on('message', this.onMessage)
+	}
+
+	onMessage = (message) => {
+		console.log("Message received: " + message);
+	}
+
+	onConnect = (event) => {
+        this.doSend("Hi servert..");
+	}
+	
+	doSend(message) {
+        this.socket.emit('message', message);
+	}
+}
